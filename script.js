2d74cdb5f62d5fc0e162379b0cd537084f2ee93b
Christian Suchanek <christian.suchanek@gmail.com>
2023-08-12 17:03:41 +0200

    add laguage support for the currencies

--- script.js
+++ script.js
@@ -1,81 +1,73 @@
-
+let browserLanguage = navigator.language || navigator.userLanguage;
+browserLanguage = browserLanguage.split("-")[1];
 
 
 function calculate(e) {
   let exchangeRateInput = document.getElementById("exchangeRate");
-let amountInput = document.getElementById("amount");
-    const resultElement = document.getElementById("result");
-    const exchangeRate = parseFloat(exchangeRateInput.value);
-    const amount = parseFloat(amountInput.value);
+  let amountInput = document.getElementById("amount");
+  const resultElement = document.getElementById("result");
+  const exchangeRate = parseFloat(exchangeRateInput.value);
+  const amount = parseFloat(amountInput.value.replace(/,/g, '.'));
+  if (isNaN(exchangeRate) || isNaN(amount)) {
+    resultElement.textContent = "Bitte gültige Zahlen eingeben.";
+  } else {
+    const convertedAmount = amount * exchangeRate;
+    resultElement.textContent = `${convertedAmount.toFixed(2).replace(".", ",")} €`;
+    localStorage.setItem("kurs", exchangeRate);
+  }
+}
 
-    if (isNaN(exchangeRate) || isNaN(amount)) {
-        resultElement.textContent = "Bitte gültige Zahlen eingeben.";
-    } else {
-        const convertedAmount = amount * exchangeRate;
-        resultElement.textContent = `${convertedAmount.toFixed(2).replace(".", ",")} €`;
-        localStorage.setItem("kurs", exchangeRate);
-    }
-}
-function getParameterFromURL() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const numberString = urlParams.get("kurs");
-    const numberWithPoint = numberString.replace(",", ".");
-    const parsedNumber = parseFloat(numberWithPoint);
-    if (!isNaN(parsedNumber)) {
-        return parsedNumber;
-    } else {
-        return null;
-    }
-}
+
 function validateInput(event) {
-    const inputValue = event.target.value;
-    event.target.value = inputValue.replace(/[^0-9,.]/g, '');
-    const firstCommaIndex = inputValue.indexOf(',');
-    if (firstCommaIndex !== -1) {
-        const commaSubstring = inputValue.substring(firstCommaIndex + 1);
-        const commaReplacement = commaSubstring.replace(/,/g, '');
-        event.target.value = inputValue.substring(0, firstCommaIndex + 1) + commaReplacement;
-        event.target.value = event.target.value.replace(/\./g, ',');
-    } else {
-        event.target.value = event.target.value.replace(/\./g, ',');
-    }
-    calculate();
+  const inputElement = document.getElementById("amount");
+  let inputValue = inputElement.value;
+  inputValue = inputValue.replace(/[^\d,.]/g, '');
+  inputValue = inputValue.replace(/,/g, (match, offset) => (offset === inputValue.indexOf(',') ? match : ''));
+  inputElement.value = inputValue;
+  calculate();
 }
 
-
-function loadExchangeRate(quote) {
-  fetch('api.php')
-  .then(response => response.json())
-  .then(data => {
-    const result = JSON.parse(data);
-    document.getElementById("exchangeRate").value = 1/parseFloat(result.quotes['EUR'+quote]);
+async function loadExchangeRate(quote) {
+  const allKurs = localStorage?.getItem("allKurs");
+  if (allKurs !== null) {
+    const result = JSON.parse(allKurs);
+    document.getElementById("exchangeRate").value = 1 / parseFloat(result.quotes['EUR' + quote]);
     calculate();
-})
-  .catch(error => console.error('Fehler beim Abrufen der Daten:', error));
+  } else {
 
+    await fetch('api.php')
+      .then(response => response.json())
+      .then(data => {
+        const result = JSON.parse(data);
+        localStorage.setItem("allKurs", data);
+        document.getElementById("exchangeRate").value = 1 / parseFloat(result.quotes['EUR' + quote]);
+        calculate();
+      })
+      .catch(error => console.error('Fehler beim Abrufen der Daten:', error));
+
+  }
 }
 
 function handleButtonClick(event) {
-    const buttonValue = event.target.textContent;
-    const amountInput = document.getElementById("amount");
-    if (buttonValue === "C") {
-        amountInput.value = "";
-        document.getElementById("result").textContent = "0 €";
-        return;
-    }
-    amountInput.value += buttonValue;
-    validateInput(event);
+  const buttonValue = event.target.textContent;
+  const amountInput = document.getElementById("amount");
+  if (buttonValue === "C") {
+    amountInput.value = "";
+    document.getElementById("result").textContent = "0 €";
+    return;
+  }
+  amountInput.value += buttonValue;
+  validateInput(event);
 }
 
 window.addEventListener("DOMContentLoaded", function () {
-    const exchangeRateInput = document.getElementById("exchangeRate");
-    const amountInput = document.getElementById("amount");
-    exchangeRateInput.addEventListener("input", calculate);
-    amountInput.addEventListener("input", validateInput);
-    const calculatorButtons = document.querySelectorAll(".calculator button");
-    calculatorButtons.forEach(button => {
-        button.addEventListener("click", handleButtonClick);
-    });
+  const exchangeRateInput = document.getElementById("exchangeRate");
+  const amountInput = document.getElementById("amount");
+  exchangeRateInput.addEventListener("input", calculate);
+  const calculatorButtons = document.querySelectorAll(".calculator button");
+  calculatorButtons.forEach(button => {
+    button.addEventListener("click", handleButtonClick);
+  });
 });
 
 
@@ -95,43 +87,70 @@ function fetchFileFromCache(file) {
 }
 
 const datas = ['currencies'];
-for( savedata of datas ){
- if(!fetchFileFromCache(savedata)){
-  fetch('currencies/'+ savedata + '.json')
-  .then(response => response.json())
-  .then(data => {
-    caches.open('data-cache').then(cache => {
-      cache.put(savedata + '.json', new Response(JSON.stringify(data)));
-     });
-  })
-  .catch(error => console.error('Fehler beim Abrufen der JSON-Datei:', error));
- }
+for (savedata of datas) {
+  if (!fetchFileFromCache(savedata)) {
+    fetch('currencies/' + savedata + '.json')
+      .then(response => response.json())
+      .then(data => {
+        caches.open('data-cache').then(cache => {
+          cache.put(savedata + '.json', new Response(JSON.stringify(data)));
+        });
+      })
+      .catch(error => console.error('Fehler beim Abrufen der JSON-Datei:', error));
+  }
 }
 
 
 
-  fetch('manifest.json')
-    .then(response => response.json())
-    .then(manifest => {
-      const quotesElement = document.getElementById('quotes');
-      const saveQuotes =  localStorage?.getItem("quotes");
-      for(wae in manifest.quotes){
+fetch('manifest.json')
+  .then(response => response.json())
+  .then(manifest => {
+    const quotesElement = document.getElementById('quotes');
+    const saveQuotes = localStorage?.getItem("quotes");
+    let waes = manifest.quotes;
+    if (manifest.translatetQuotes[browserLanguage] !== undefined) {
+      waes = manifest.translatetQuotes[browserLanguage];
+    }
 
-        const option = document.createElement('option');
-        option.textContent = manifest.quotes[wae];
-        option.value = wae;
-        if(saveQuotes === wae){
-          option.selected = true;
-          loadExchangeRate(wae);
-        }
-        quotesElement.appendChild(option);
+    for (wae in waes) {
+
+      const option = document.createElement('option');
+      option.textContent = waes[wae];
+      option.value = wae;
+      if (saveQuotes === wae) {
+        option.selected = true;
+        loadExchangeRate(wae);
       }
-     document.getElementById('quotes').addEventListener('change', function() {
+      quotesElement.appendChild(option);
+    }
+    document.getElementById('quotes').addEventListener('change', async function () {
       localStorage.setItem("quotes", this.value);
-      loadExchangeRate(this.value);
-     });
-
-    })
-    .catch(error => {
-      console.error('Fehler beim Laden des Manifests:', error);
+     await loadExchangeRate(this.value);
     });
+
+  })
+  .catch(error => {
+    console.error('Fehler beim Laden des Manifests:', error);
+  });
+
+
+//   // Überprüfe, ob der Browser die Geolocation unterstützt
+// if ("geolocation" in navigator) {
+//   // Frage den Nutzer nach der Berechtigung zur Standortabfrage
+//   navigator.geolocation.getCurrentPosition(
+//     function(position) {
+//       const latitude = position.coords.latitude;
+//       const longitude = position.coords.longitude;
+
+//       // Hier kannst du die Koordinaten verwenden, um den Standort zu ermitteln
+//       // Du könntest z.B. eine API für geografische Informationen verwenden
+
+//       console.log("Latitude: " + latitude + ", Longitude: " + longitude);
+//     },
+//     function(error) {
+//       console.error("Fehler bei der Geolocation: " + error.message);
+//     }
+//   );
+// } else {
+//   console.error("Geolocation wird nicht unterstützt.");
+// }
